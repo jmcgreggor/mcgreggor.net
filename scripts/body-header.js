d314ced1b30c41308c6111c6d7a6662b5fded0b3
james.mcgreggor <jpatrickmcgreggor@gmail.com>
2018-05-25 13:34:57 -0400

    MCG-4 Create recommendations page.

--- scripts/body-header.js
+++ scripts/body-header.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
         $('[data-header-index="header-index"]').load("partials/header-index.html");
         $('[data-header-about="header-about"]').load("partials/header-about.html");
+        $('[data-header-recommend="header-recommend"]').load("partials/header-recommend.html");
 });
--- scripts/body-main.js
+++ scripts/body-main.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
         $('[data-main-index="main-index"]').load("partials/body-index-main.html");
         $('[data-main-about="main-about"]').load("partials/body-about-main.html");
+        $('[data-main-recommend="main-recommend"]').load("partials/body-recommend-main.html");
 });
