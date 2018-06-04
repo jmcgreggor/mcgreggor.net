21de990c92adf947b12be7ce96446aece04f911b
james.mcgreggor <jpatrickmcgreggor@gmail.com>
2018-06-04 18:04:04 -0400

    MCG-7 Update to use CircleType.js and consolidate js files

--- scripts/body-main.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(() => {
-        $('[data-main-index="main-index"]').load("partials/body-index-main.html");
-        $('[data-main-about="main-about"]').load("partials/body-about-main.html");
-        $('[data-main-recommend="main-recommend"]').load("partials/body-recommend-main.html");
-});
