8c5de993857923beac1f448882640837e09c35cd
james.mcgreggor <jpatrickmcgreggor@gmail.com>
2018-08-08 12:40:03 -0400

    MCG-17 Massive refactor of CSS and HTML

--- scripts/app.js
+++ scripts/app.js
@@ -7,35 +7,26 @@
 
   $(document).ready(() => {
 
-       $('[data-navbar="navbar-div"]').load("partials/navbar.html");
+        $('[data-navbar="navbar-div"]').load("partials/navbar.html");
 
-            if (UserDevice == "large-viewport") {
+        if (UserDevice == "large-viewport") {
 
-                $('[data-header-index="header-index"]').load("partials/header-index.html", () => {
+            $('[data-header-index="header-index"]').load("partials/header-index.html", () => {
+               const myCircleType = new CircleType(document.getElementById('bendyArc'));
+               myCircleType.radius(400).dir(-1);
+            });
 
-                   const myCircleType = new CircleType(document.getElementById('bendyArc'));
+            $('header').css("background-attachment","fixed");
 
-                   myCircleType.radius(300).dir(-1);
+        } else {
 
-                });
-
-                $('header').css("background-attachment","fixed");
-
-            } else {
-
-                $('[data-header-index="header-index"]').load("partials/header-index-mobile.html", () => {
-
-                  $(document).click(function (event) {
-
-                      $('.navbar-collapse').collapse('hide');
-
-                  });
-
-                  $('.navbar-brand').hide();
-
-                });
-
-            };
+            $('[data-header-index="header-index"]').load("partials/header-index-mobile.html", () => {
+              //TODO: Fix nav collapse on mobile
+              $(document).click(function (event) {
+                  $('.navbar-collapse').collapse('hide');
+              });
+            });
+        };
 
         $('[data-header-about="header-about"]').load("partials/header-about.html");
         $('[data-header-recommend="header-recommend"]').load("partials/header-recommend.html");
@@ -45,5 +36,29 @@
         $('[data-footer="footer-div"]').load("partials/footer.html");
 
   });
+
+  $(document).on('click',function(event){
+
+       $('.collapse').collapse('hide');
+
+       //TODO: Fix navbar hamburger icon to reset state when tapped outside
+
+       // $('.icon-bar').css(
+       //   "&:nth-child(1) {transform: translateY(-6px) rotate(-45deg);}",
+       //   "&:nth-child(2) {opacity: 0; transform: translateX(100%);}",
+       //   "&:nth-child(3) {transform: translateY(6px) rotate(45deg);}"
+       // );
+
+       //$(".navbar").removeClass('on');
+       //$(".navbar").removeClass('active');
+       //$(".navbar").toggleClass('off');
+       //$(".navbar-toggle").removeClass('on');
+       //$(".navbar-toggle").removeClass('active');
+       //$(".navbar-toggle").toggleClass('off');
+       //$(".icon-bar").removeClass('on');
+       //$(".icon-bar").removeClass('active');
+       //$(".icon-bar").toggleClass('off');
+  });
+
 
 })(window);
