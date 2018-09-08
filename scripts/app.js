81218bd84312def4e53d3ba506744ade91edec3a
james.mcgreggor <jpatrickmcgreggor@gmail.com>
2018-09-08 12:17:22 -0400

    MCG-21 Add im fade in class, add page styles, add addtl pages

--- scripts/app.js
+++ scripts/app.js
@@ -11,20 +11,20 @@
 
         if (UserDevice == "large-viewport") {
 
-            $("media-test-divider").addClass("media-large-divider");
-            $("media-test-para-divider").addClass("media-large-para-divider");
-
             $('[data-header-index="header-index"]').load("partials/header-index.html", () => {
                const myCircleType = new CircleType(document.getElementById('bendyArc'));
                myCircleType.radius(400).dir(-1);
             });
             $('[data-header-about="header-about"]').load("partials/header-about.html");
             $('[data-header-recommend="header-recommend"]').load("partials/header-recommend.html");
+            $('[data-header-strengths="header-strengths"]').load("partials/header-strengths.html");
+            $('[data-header-thoughts="header-thoughts"]').load("partials/header-thoughts.html");
 
-            $('header').css("background-attachment","fixed");
 
             $("media-test-divider").addClass("media-large-divider");
             $("media-test-para-divider").addClass("media-large-para-divider");
+
+            $('header').css("background-attachment","fixed");
 
         } else {
 
@@ -36,12 +36,40 @@
             });
             $('[data-header-about="header-about"]').load("partials/header-about-mobile.html");
             $('[data-header-recommend="header-recommend"]').load("partials/header-recommend-mobile.html");
+            $('[data-header-strengths="header-strengths"]').load("partials/header-strengths-mobile.html");
+            $('[data-header-thoughts="header-thoughts"]').load("partials/header-thoughts-mobile.html");
+
         };
 
         $('[data-main-index="main-index"]').load("partials/body-index-main.html");
         $('[data-main-about="main-about"]').load("partials/body-about-main.html");
         $('[data-main-recommend="main-recommend"]').load("partials/body-recommend-main.html");
+        $('[data-main-strengths="main-strengths"]').load("partials/body-strengths-main.html");
+        $('[data-main-thoughts="main-thoughts"]').load("partials/body-thoughts-main.html");
+
         $('[data-footer="footer-div"]').load("partials/footer.html");
+
+        function showImages(el) {
+          var windowHeight = jQuery( window ).height();
+          $(el).each(function(){
+            var thisPos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (topOfWindow + windowHeight - 200 > thisPos ) {
+                $(this).addClass("fadeIn");
+            }
+          });
+        }
+
+        // if the image in the window of browser when the page is loaded, show that image
+        $(document).ready(function(){
+            showImages('.star');
+        });
+
+        // if the image in the window of browser when scrolling the page, show that image
+        $(window).scroll(function() {
+            showImages('.star');
+        });
 
   });
 
